8787bd4136e524803ed3656022e780cfdcb5a051
gomez-fernando <ferdgomez@hotmail.es>
2022-04-26 23:17:00 +0100

    searched todos in process

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 // import './App.css';
 
-const todos = [
+const defaultTodos = [
   { text: 'Leer un libro', completed: true },
   { text: 'Ir a la piscina', completed: false },
   { text: 'Equitación', completed: false },
@@ -14,13 +14,32 @@ const todos = [
 ];
 
 function App() {
+  const [todos , setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if(!searchValue.length >= 1) {
+    searchedTodos = todos;
+  }
+
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter 
+        total={totalTodos}
+        completed={completedTodos}
+      />
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
-        {todos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,9 +1,55 @@
-import React from 'react';
-import './TodoSearch.css';
+import React from "react";
+import "./TodoSearch.css";
 
-function TodoSearch() {
+// class Componente extends React.Component {
+//   constructor() {
+//     this.state = {
+//       nombre: 'Jaime'
+//     }
+//   }
+
+//   render(){
+//     return (
+//       <div onClick={() => this.setState({nombre: 'Enrique'})}>{this.state.nombre}</div>
+//     )
+//   }
+// }
+
+function TodoSearch({searchValue, setSearchValue}) {
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  }
+
+  // return [
+  //   <input 
+  //     className="TodoSearch" 
+  //     placeholder="Libro" 
+  //     onChange = {onSearchValueChange}
+  //     // onChange = {() => setEstado('Miguel')}
+
+  //   />,
+  //   <p>
+  //     {searchValue}
+  //   </p>
+
+  // ];
+
   return (
-    <input className="TodoSearch" placeholder="Libro" />
+    // <React.Fragment>
+      <input 
+        className="TodoSearch" 
+        placeholder="Libro" 
+        value={searchValue}
+        onChange = {onSearchValueChange}
+        // onChange = {() => setSearchValue('Miguel')}
+
+      />
+      // <p>
+      //   {searchValue}
+      // </p>
+    // </React.Fragment>
+
   );
 }
 
