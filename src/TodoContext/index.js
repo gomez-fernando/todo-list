ae11ec4e9836aafee0a48ae34abf078c2ef5aa11
gomez-fernando <ferdgomez@hotmail.es>
2022-05-02 13:48:59 +0100

    ok

---
+++ src/TodoContext/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TodoContext = React.createContext();
+
+function TodoProvider(props){
+  return (
+    <TodoContext.Provider value={{
+      
+    }}>
+      {props.children}
+    </TodoContext.Provider>
+
+  )
+}
+
+<TodoContext.Consumer></TodoContext.Consumer>
